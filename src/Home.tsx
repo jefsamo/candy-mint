f258dff7f3f286f482fb4a38031a6eb0a15820d0
Olawale Shopeyin <shopeyinwale@gmail.com>
2022-07-25 14:31:03 +0100

    UI tweaks

--- src/Home.tsx
+++ src/Home.tsx
@@ -37,7 +37,7 @@ const ConnectButton = styled(WalletDialogButton)`
   height: 60px;
   margin-top: 10px;
   margin-bottom: 5px;
-  background: linear-gradient(180deg, #604ae5 0%, #813eee 100%);
+  background: #00a551;
   color: white;
   font-size: 16px;
   font-weight: bold;
